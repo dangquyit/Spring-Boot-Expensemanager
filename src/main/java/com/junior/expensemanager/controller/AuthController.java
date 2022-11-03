4cf09300dc5e3eb5fb9ab3682977344cbb63685d
Nguyen Dang Quy <dangquyitt@gmail.com>
2022-11-03 11:21:16 +0700

    remove spring security

--- src/main/java/com/junior/expensemanager/controller/AuthController.java
+++ src/main/java/com/junior/expensemanager/controller/AuthController.java
@@ -1,46 +1,28 @@
 package com.junior.expensemanager.controller;
 
 import com.junior.expensemanager.dto.UserDTO;
-import com.junior.expensemanager.repository.UserRepository;
-import com.junior.expensemanager.service.UserService;
-import com.junior.expensemanager.validator.UserValidator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthController {
-    @Autowired
-    private UserValidator userValidator;
-
-    @Autowired
-    private UserService userService;
-
     @GetMapping(value = {"/login", "/"})
-    public String showLoginPage() {
-
+    public String showLogin() {
         return "login";
     }
 
     @GetMapping(value = {"/register"})
-    public String showRegisterPage(Model model) {
-        model.addAttribute("user", new UserDTO());
-        return "register";
+    public String showRegister(Model model) {
+    model.addAttribute("user", new UserDTO());
+    return "register";
     }
 
-    @PostMapping(value = {"/register"})
-    public String register(@ModelAttribute("user") UserDTO userDTO, Model model, BindingResult result) {
-        userValidator.validate(userDTO, result);
-        if(result.hasErrors()) {
-            return "register";
-        }
-        userService.save(userDTO);
+    @PostMapping(value = {"/save-or-update-user"})
+    public String saveOrUpdateUser(@ModelAttribute("user") UserDTO userDTO, Model model) {
         model.addAttribute("successMsg", true);
         return "login";
     }
--- src/main/java/com/junior/expensemanager/service/UserService.java
+++ src/main/java/com/junior/expensemanager/service/UserService.java
@@ -7,31 +7,23 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 public class UserService {
     @Autowired
-    private ModelMapper modelMapper;
+    UserRepository userRepository;
 
     @Autowired
-    private UserRepository userRepository;
-
-    public void save(UserDTO userDTO) {
-        User user = mapToEntity(userDTO);
-        userRepository.save(user);
-    }
+    ModelMapper modelmapper;
 
     private User mapToEntity(UserDTO userDTO) {
-        return modelMapper.map(userDTO, User.class);
+        return modelmapper.map(userDTO, User.class);
     }
 
     private UserDTO mapToDTO(User user) {
-        return modelMapper.map(user, UserDTO.class);
+        return modelmapper.map(user, UserDTO.class);
     }
-    public List<UserDTO> findByEmail(String email) {
-        List<User> users = userRepository.findByEmail(email);
-        return users.stream().map(this::mapToDTO).collect(Collectors.toList());
+
+    public void save(UserDTO userDTO) {
+        userRepository.save(mapToEntity(userDTO));
     }
 }
