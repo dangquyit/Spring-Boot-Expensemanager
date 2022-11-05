103f85e88380815016904e8004633eb54d09bd11
Nguyen Dang Quy <dangquyitt@gmail.com>
2022-11-05 16:56:49 +0700

    using spring security add method logout

--- src/main/java/com/junior/expensemanager/config/CustomSecurityConfig.java
+++ src/main/java/com/junior/expensemanager/config/CustomSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
 
 @Configuration
 public class CustomSecurityConfig {
@@ -28,6 +30,11 @@ public class CustomSecurityConfig {
                 .defaultSuccessUrl("/expenses")
                 .usernameParameter("email")
                 .passwordParameter("password")
+                .and()
+                .logout().invalidateHttpSession(true).clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout=true")
+                .permitAll()
 
         ;
         return http.build();
